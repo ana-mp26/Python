018fb8fd4715a6b7c50306f9a2365e288dc3a407
ana-mp26 <79088264+ana-mp26@users.noreply.github.com>
2021-07-23 17:05:23 +0200

    Add files via upload

---
+++ ## Score calculator.py
@@ -0,0 +1,62 @@
+## Score calculator
+
+lloyd = {
+  "name": "Lloyd",
+  "homework": [90.0, 97.0, 75.0, 92.0],
+  "quizzes": [88.0, 40.0, 94.0],
+  "tests": [75.0, 90.0]
+}
+alice = {
+  "name": "Alice",
+  "homework": [100.0, 92.0, 98.0, 100.0],
+  "quizzes": [82.0, 83.0, 91.0],
+  "tests": [89.0, 97.0]
+}
+tyler = {
+  "name": "Tyler",
+  "homework": [0.0, 87.0, 75.0, 22.0],
+  "quizzes": [0.0, 75.0, 78.0],
+  "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+# AVERAGE FUNCTION
+def average(numbers):
+  total= sum(numbers)
+  total= float(total)
+  total = total/len(numbers)
+  return total
+
+# AVERAGE SCORE OF A STUDENT
+def get_average(student):
+  homework = average(student["homework"])
+  quizzes = average(student["quizzes"])
+  tests = average(student["tests"])
+  return 0.1*homework + 0.3*quizzes + 0.6*tests
+
+# LETTER GRADE OF A STUDENT GIVEN THE AVERAGE SCORE
+def get_letter_grade(score):
+  if score >= 90:
+    return "A"
+  elif score >=80:
+    return "B"
+  elif score >= 70:
+    return "C"
+  elif score >= 60:
+    return "D"
+  else:
+    return "F"
+
+print (get_letter_grade(get_average(lloyd)))
+
+#AVERAGE GRADE OF THE STUDENTS
+def get_class_average(class_list):
+  results= []
+  for student in class_list:
+    results.append(get_average(student))
+  return(average(results))
+
+
+students= [alice, lloyd, tyler]
+print (get_class_average(students)) # find the average grade of the class
+print (get_letter_grade(get_class_average(students))) # determine the letter grade for the class's average
